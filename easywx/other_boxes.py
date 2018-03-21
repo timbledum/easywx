7531977365bebac391f57715634be6bef4f2ba01
timbledum <timbledum@gmail.com>
2018-03-21 20:40:09 +1300

    Remember to include new file for easygui definitions

---
+++ easywx/other_boxes.py
@@ -0,0 +1,39 @@
+
+def textbox(msg='', title=' ', text='', codebox=False, callback=None, run=True):
+    pass
+
+
+def integerbox(msg='', title=' ', default=None, lowerbound=0, upperbound=99, images=None, root=None):
+    pass
+
+
+def multenterbox(msg='Fill in values for the fields.', title=' ', fields=[], values=[], callback=None, run=True):
+    pass
+
+
+def enterbox(msg='Enter something.', title=' ', default='', strip=True, image=None, root=None):
+    pass
+
+
+def exceptionbox(msg=None, title=None):
+    pass
+
+
+def choicebox(msg='Pick an item', title='', choices=[], preselect=0, callback=None, run=True):
+    pass
+
+
+def codebox(msg='', title=' ', text=''):
+    pass
+
+
+def passwordbox(msg='Enter your password.', title=' ', default='', image=None, root=None):
+    pass
+
+
+def multpasswordbox(msg='Fill in values for the fields.', title=' ', fields=(), values=(), callback=None, run=True):
+    pass
+
+
+def multchoicebox(msg='Pick an item', title='', choices=[], preselect=0, callback=None, run=True):
+    pass
