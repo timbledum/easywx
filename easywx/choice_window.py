a986579ce12a570ec83dbdaf4aebaae83dea852d
timbledum <timbledum@gmail.com>
2018-04-15 15:36:06 +1200

    Add functionality and class for choiceboxes

---
+++ easywx/choice_window.py
@@ -0,0 +1,60 @@
+import wx
+
+
+class ChoiceWindow(wx.Frame):
+    def __init__(self,
+                 msg=None,
+                 title=None,
+                 choices=['Yes', 'No', 'Maybe'],
+                 preselect=0,
+                 multiple=False,
+                 settings=None,):
+        super().__init__(None)
+
+        self.SetTitle(title)
+        self.result = None
+        self.choices = choices
+
+        panel = wx.Panel(self)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        text_label = wx.StaticText(panel, -1, label=msg)
+        text_label.Wrap(settings.min_width)
+        sizer.Add(text_label, **settings.sizer_settings)
+
+        wx_multi = wx.LB_EXTENDED if multiple else wx.LB_SINGLE
+        self.list_box = wx.ListBox(panel, -1, choices=self.choices,
+                                   style=wx_multi)
+
+        self.list_box.SetSelection(preselect)
+        sizer.Add(self.list_box, **settings.sizer_settings)
+
+        self.buttons = [wx.Button(panel, -1, text) for text in
+                        ['Cancel', 'OK']]
+
+        for button in self.buttons:
+            button.Bind(wx.EVT_BUTTON, self.button_press)
+
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        for button in self.buttons:
+            button_sizer.Add(button, **settings.sizer_settings)
+
+        sizer.Add(button_sizer, **settings.sizer_settings)
+
+        sizer.SetSizeHints(self)
+        panel.SetSizerAndFit(sizer)
+
+        panel.Layout()
+        self.Show(True)
+
+    def button_press(self, event):
+
+        button_clicked = self.buttons.index(event.EventObject)
+        result = self.list_box.GetSelections()
+        if button_clicked == 1:
+            self.result = [self.choices[ind] for ind in result]
+        else:
+            self.result = None
+        self.Close()
--- easywx/other_boxes.py
+++ easywx/other_boxes.py
@@ -1,17 +1,45 @@
+import wx
+from .choice_window import ChoiceWindow
+from .settings import Settings
 
-def textbox(msg='', title=' ', text='', codebox=False, callback=None, run=True):
+
+def textbox(
+    msg="", title=" ", text="", codebox=False, callback=None, run=True
+):
     pass
 
 
-def integerbox(msg='', title=' ', default=None, lowerbound=0, upperbound=99, images=None, root=None):
+def integerbox(
+    msg="",
+    title=" ",
+    default=None,
+    lowerbound=0,
+    upperbound=99,
+    images=None,
+    root=None,
+):
     pass
 
 
-def multenterbox(msg='Fill in values for the fields.', title=' ', fields=[], values=[], callback=None, run=True):
+def multenterbox(
+    msg="Fill in values for the fields.",
+    title=" ",
+    fields=[],
+    values=[],
+    callback=None,
+    run=True,
+):
     pass
 
 
-def enterbox(msg='Enter something.', title=' ', default='', strip=True, image=None, root=None):
+def enterbox(
+    msg="Enter something.",
+    title=" ",
+    default="",
+    strip=True,
+    image=None,
+    root=None,
+):
     pass
 
 
@@ -19,21 +47,57 @@ def exceptionbox(msg=None, title=None):
     pass
 
 
-def choicebox(msg='Pick an item', title='', choices=[], preselect=0, callback=None, run=True):
+def choicebox(
+    msg="Pick an item",
+    title="",
+    choices=['Option1', 'Option2', 'Option3'],
+    preselect=0,
+    callback=None,
+    run=True,
+):
+        settings = Settings()
+
+        app = wx.App()
+        frame = ChoiceWindow(msg=msg, title=title, choices=choices,
+                             settings=settings, multiple=False)
+        app.MainLoop()
+
+        return frame.result[0] if frame.result else None
+
+
+def codebox(msg="", title=" ", text=""):
     pass
 
 
-def codebox(msg='', title=' ', text=''):
+def passwordbox(
+    msg="Enter your password.", title=" ", default="", image=None, root=None
+):
     pass
 
 
-def passwordbox(msg='Enter your password.', title=' ', default='', image=None, root=None):
+def multpasswordbox(
+    msg="Fill in values for the fields.",
+    title=" ",
+    fields=(),
+    values=(),
+    callback=None,
+    run=True,
+):
     pass
 
 
-def multpasswordbox(msg='Fill in values for the fields.', title=' ', fields=(), values=(), callback=None, run=True):
-    pass
+def multchoicebox(
+    msg="Pick an item",
+    title="",
+    choices=[],
+    preselect=0,
+    callback=None,
+    run=True,
+):
+        settings = Settings()
 
-
-def multchoicebox(msg='Pick an item', title='', choices=[], preselect=0, callback=None, run=True):
-    pass
+        app = wx.App()
+        frame = ChoiceWindow(msg=msg, title=title, choices=choices,
+                             settings=settings, multiple=True)
+        app.MainLoop()
+        return frame.result
