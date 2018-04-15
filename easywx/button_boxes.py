552663efaa08b26671aa03646d3dc1c502b6a6c6
timbledum <timbledum@gmail.com>
2018-04-15 16:48:18 +1200

    Flesh out msgbox function

--- easywx/button_boxes.py
+++ easywx/button_boxes.py
@@ -5,61 +5,67 @@ from .button_window import ButtonWindow
 from .settings import Settings
 
 
-def indexbox(msg='Shall I continue?',
-             title='',
-             choices=('Yes', 'No'),
-             images=None):
+def indexbox(
+    msg="Shall I continue?", title="", choices=("Yes", "No"), images=None
+):
 
     settings = Settings()
 
     app = wx.App()
-    frame = ButtonWindow(msg=msg, title=title, choices=choices, images=images,
-                         settings=settings)
+    frame = ButtonWindow(
+        msg=msg, title=title, choices=choices, images=images, settings=settings
+    )
     app.MainLoop()
     return frame.result
 
 
-def buttonbox(msg='',
-              title='',
-              choices=('Button[1]', 'Button[2]', 'Button[3]'),
-              images=None):
+def buttonbox(
+    msg="",
+    title="",
+    choices=("Button[1]", "Button[2]", "Button[3]"),
+    images=None,
+):
 
     return choices[indexbox(**locals())]
 
 
-def ynbox(msg='Shall I continue?',
-          title='',
-          choices=['Yes', 'No']):
+def ynbox(msg="Shall I continue?", title="", choices=["Yes", "No"]):
 
     return indexbox(**locals())
 
 
-def ccbox(msg='Shall I continue?',
-          title='',
-          choices=['C[o]ntinue', 'C[a]ncel']):
+def ccbox(
+    msg="Shall I continue?", title="", choices=["C[o]ntinue", "C[a]ncel"]
+):
 
     return indexbox(**locals())
 
 
-def boolbox(msg='Shall I continue?',
-            title='',
-            choices=('[Y]es', '[N]o'),
-            images=None):
+def boolbox(
+    msg="Shall I continue?", title="", choices=("[Y]es", "[N]o"), images=None
+):
 
     return indexbox(**locals()) == 0  # should also return True if cancelled
 
 
-def msgbox(msg='(Your message goes here)',
-           title=' ',
-           ok_button='OK',
-           image=None,
-           root=None):
-    
-    return None
+def msgbox(
+    msg="(Your message goes here)",
+    title=" ",
+    ok_button="OK",
+    image=None,
+    root=None,
+):
+
+    indexbox(msg=msg, title=title, choices=(ok_button,), images=image)
+    return ok_button
+
 
 if __name__ == "__main__":
 
-    button_boxes = [func for func in globals().values() if
-                    isinstance(func, types.FunctionType)]
+    button_boxes = [
+        func
+        for func in globals().values()
+        if isinstance(func, types.FunctionType)
+    ]
     for func in button_boxes:
         func()
